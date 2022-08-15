b1c61eeb732d0df5fbe0f240ced52205e7ec124e
odulaja philip <odulajaphilip@gmail.com>
2022-08-15 20:02:04 +0100

    modelSerializers

--- imdb/watchmate/api/serializers.py
+++ imdb/watchmate/api/serializers.py
@@ -1,18 +1,35 @@
 from rest_framework import serializers
 from watchmate.models import Movie
 
+
+
+class MovieSerializers(serializers.Modelserializers):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+    
+    def validate(self , data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Name and description must not be same')
+        return data
+    
+    def validate_name(self, value):
+        if len(value) < 2 :
+            raise serializers.ValidationError("name length most be greater than two")
+
+    
 """
 field level validation
-"""
+
 def validator_name(value):
     if len(value) < 2 :
         raise serializers.ValidationError("name length most be greater than two")
 
 class MovieSerializers(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validator=[validator_name])
+    name = serializers.CharField(validators=[validator_name])
     description = serializers.CharField()
-    active = serializers.BooleanField()
+    active = serializers.BooleanField(default=True)
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
@@ -24,10 +41,11 @@ class MovieSerializers(serializers.Serializer):
         instance.save()
         return instance
 
-    """
+   
     object level validation
-    """
+  
     def validate(self , data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('Name and description must not be same')
         return data
+"""
