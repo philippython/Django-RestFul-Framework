4b8d73014caf61461366c6c20b82931196e9f8ce
odulaja philip <odulajaphilip@gmail.com>
2022-08-16 21:27:13 +0100

    created super user

--- imdb/watchmate/api/urls.py
+++ imdb/watchmate/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from watchmate.api.views import WatchListView, WatchDetailsView, StreamPlatformListView
+from watchmate.api.views import WatchListView, WatchDetailsView, StreamPlatformListView, StreamPlatformDetailView
 # from watchmate.api.views import movie_list, movie_details
 
 urlpatterns = [
-        path('watchlist/', WatchListView.as_view(), name='movie_list'),
+        path('watchlist/', WatchListView.as_view(), name='watchlist'),
         path('watchdetails/<int:pk>/', WatchDetailsView.as_view(), name='single_watchlist'),
         path('streamplatform/list', StreamPlatformListView.as_view(), name='streamplatform_list'),
-        path('streamplatform/details/<int:pk>', )
+        path('streamplatform/details/<int:pk>',StreamPlatformDetailView.as_view(), name='single_streamplatform' )
 ]
--- imdb/watchmate/models.py
+++ imdb/watchmate/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
-    about = models.CharField(max_lenght=200)
+    about = models.CharField(max_length=200)
     website = models.URLField(max_length=100)
 
     def __str__(self):
