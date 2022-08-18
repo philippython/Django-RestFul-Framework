0281bfdba8b60eae85b336025d2789711b3c3430
odulaja philip <odulajaphilip@gmail.com>
2022-08-18 23:44:12 +0100

    added review model to database

--- imdb/watchmate/models.py
+++ imdb/watchmate/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
@@ -18,3 +18,6 @@ class WatchList(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+class Review(models.Model):
+    rating =models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
