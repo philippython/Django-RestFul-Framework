7320bee5b91ae5bf0fe02b20e24ede22bcba7069
odulaja philip <odulajaphilip@gmail.com>
2022-08-20 18:30:08 +0100

    added netnaija to stream platforms

--- imdb/watchmate/api/views.py
+++ imdb/watchmate/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.generics import (CreateAPIView, ListAPIView,
                                      RetrieveUpdateDestroyAPIView)
 from rest_framework.mixins import (ListModelMixin, CreateModelMixin,
