26c8fdc18d9342de1883bb5f1db39e78f54d0a0a
odulaja philip <odulajaphilip@gmail.com>
2022-08-22 08:22:17 +0100

    updated permissions.py

--- imdb/watchmate/api/permission.py
+++ imdb/watchmate/api/permission.py
@@ -2,6 +2,6 @@ from rest_framework.permissions import BasePermission, IsAdminUser
 
 class AdminOrReadOnly(IsAdminUser):
 
-    def has_permisssions(self, request, view):
+    def has_permission(self, request, view):
         admin_permission = bool(request.user and request.user.is_staff)
-        return request.methodd == 'GET'or admin_permission
+        return request.method == 'GET' or admin_permission
