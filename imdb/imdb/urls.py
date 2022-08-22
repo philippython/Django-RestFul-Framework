f6c271dc80d2dd6dd25a6fa8343c95d5f23499c3
odulaja philip <odulajaphilip@gmail.com>
2022-08-22 04:10:48 +0100

    intro to permissions

--- imdb/imdb/urls.py
+++ imdb/imdb/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('imdb/', include('watchmate.api.urls'))
+    path('imdb/', include('watchmate.api.urls')),
+    path('api-auth', include('rest_framework.urls'))
 ]
