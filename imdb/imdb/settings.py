81d7f921fa34862c17f2ff5186faf884f9eff1ce
odulaja philip <odulajaphilip@gmail.com>
2022-08-24 02:55:19 +0100

    update settings.py

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'watchmate',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
