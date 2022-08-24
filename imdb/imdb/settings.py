079165375e19b30850de1d8031ae2cf49a68b2ac
odulaja philip <odulajaphilip@gmail.com>
2022-08-24 02:47:50 +0100

    Token Authentication

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -126,7 +126,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+   # 'DEFAULT_AUTHENTICATION_CLASSES': [
+   #      'rest_framework.authentication.BasicAuthentication',
+   #  ],
    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
