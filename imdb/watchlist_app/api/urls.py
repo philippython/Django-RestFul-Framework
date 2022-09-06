91756e1aa2f21242eba16793fc0654a8805dd767
odulaja philip <odulajaphilip@gmail.com>
2022-09-06 15:45:21 +0100

    updated urls.py

--- imdb/watchlist_app/api/urls.py
+++ imdb/watchlist_app/api/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     # path('logout', logout_view, name='logout')
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
 ]
