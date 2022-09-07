66e8c7e9c91b8cc0c4b2a68bfffa7666ba8e5128
odulaja philip <odulajaphilip@gmail.com>
2022-09-07 12:29:06 +0100

    updated urls.py

--- imdb/watchlist_app/api/urls.py
+++ imdb/watchlist_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-# from rest_framework.authtoken.views import obtain_auth_token
-# from .views import registration_view, logout_view
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import registration_view, logout_view
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
