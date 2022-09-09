1bd323135227dad241940df755c5b8a40b89cb13
odulaja philip <odulajaphilip@gmail.com>
2022-09-09 08:49:48 +0100

    updated permissions.py

--- imdb/watchmate/api/permission.py
+++ imdb/watchmate/api/permission.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 class AdminOrReadOnly(permissions.IsAdminUser):
 
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
