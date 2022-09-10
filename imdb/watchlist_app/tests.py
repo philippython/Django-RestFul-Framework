8eee87daceb24726661fe8626622fa92220366df
odulaja philip <odulajaphilip@gmail.com>
2022-09-10 22:48:59 +0100

    imported tests required modules

--- imdb/watchlist_app/tests.py
+++ imdb/watchlist_app/tests.py
@@ -1,1 +1,12 @@
-f
+from rest_framework.test import APITestCase
+from rest_framework.response import Response
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class UserRegistrationTest(APITestCase):
+    """test registration route"""
+
+    def test_register_user(self):
+
+        url = reverse('register')
