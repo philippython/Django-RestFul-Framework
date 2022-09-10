de57a714b1c8bf55610042938d8b4ff41f75816e
odulaja philip <odulajaphilip@gmail.com>
2022-09-10 23:27:14 +0100

    test1

--- imdb/watchlist_app/tests.py
+++ imdb/watchlist_app/tests.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework.response import Response
+import rest_framework
 from rest_framework import status
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -10,3 +11,12 @@ class UserRegistrationTest(APITestCase):
     def test_register_user(self):
 
         url = reverse('register')
+
+        self.assertEqual(url.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, "hp")
+
+
+
+if __name__ == '__main__':
+    rest_framework.test.main()
