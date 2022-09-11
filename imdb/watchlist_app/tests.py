fa8d632bd8e32bf53a62e9653617dd8495c1a931
odulaja philip <odulajaphilip@gmail.com>
2022-09-11 16:39:36 +0100

    registration test failed

--- imdb/watchlist_app/tests.py
+++ imdb/watchlist_app/tests.py
@@ -1,4 +1,4 @@
-from http import client
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from rest_framework import status
@@ -10,10 +10,15 @@ class RegistrationTest(APITestCase):
     def test_register_user(self):
 
         url = reverse('register')
-        data = {"name": "philip"}
+        data = {
+                "username": "testcase",
+                "email": "testcase@example.com",
+                "password1": "NewPassword@123",
+                "password2": "NewPassword@123"
+                }
 
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().name, "philip")
+        self.assertEqual(User.objects.filter(email__exact="temitayo@gmail.com"), "temitayo@gmail.com")
 
