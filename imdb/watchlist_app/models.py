50c525b2c81eb82f65d31cff0349cb7fad496e22
odulaja philip <odulajaphilip@gmail.com>
2022-09-11 06:31:53 +0100

    created Account test model

--- imdb/watchlist_app/models.py
+++ imdb/watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
@@ -8,3 +9,5 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
+class Account(models.Model):
+    name = models.CharField(max_length=25)
