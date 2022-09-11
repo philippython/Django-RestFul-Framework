67a86c917cb11fa19b687048f3532a1baa0b81a4
odulaja philip <odulajaphilip@gmail.com>
2022-09-11 06:41:14 +0100

    added watchlist_app to INSTALLED_APPS

--- imdb/imdb/settings.py
+++ imdb/imdb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'watchmate',
+    'watchlist_app',
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters'
